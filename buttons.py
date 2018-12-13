6996f686febfb609edf99a0919ac674fac30274b
Kevin Peterson <frankdeath@gmail.com>
2018-12-13 04:06:51 +0000

    Added buttons.py

---
+++ buttons.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
+#
+
+import RPi.GPIO as GPIO
+
+# Define pins
+forward=24
+backward=23
+pause=26
+
+# Ignore warnings
+GPIO.setwarnings(False)
+# Use the board numbering scheme for I/O pins
+GPIO.setmode(GPIO.BOARD)
+# Set pins 23, 24, and 26 as inputs with initial value to be pulled high (ON)
+GPIO.setup(backward, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(foreward, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pause, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Pin 25 is ground
+
+# Callback functions for buttons
+def backwardCallback(channel):
+  print("Backward button was pushed!")
+
+def forwardCallback(channel):
+  print("Forward button was pushed!")
+
+def playPauseCallback(channel):
+  print("Play/Pause button was pushed!")
+
+# Register callback functions
+GPIO.add_event_detect(backward, GPIO.FALLING,callback=backwardCallback)
+GPIO.add_event_detect(backward, GPIO.FALLING,callback=backwardCallback)
+GPIO.add_event_detect(backward, GPIO.FALLING,callback=backwardCallback)
+
+# pseudo main loop
+message = input("Press enter to quit\n")
+
+# Clean up
+GPIO.cleanup()
+
+
