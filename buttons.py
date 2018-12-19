b362067d4dc0066f4537925eb903f9f9252c81a5
Kevin Peterson <frankdeath@gmail.com>
2018-12-19 04:39:28 +0000

    Added sapi.py (screenly api).  Implemented forward and backward buttons.

--- buttons.py
+++ buttons.py
@@ -4,6 +4,7 @@
 #
 
 import RPi.GPIO as GPIO
+import sapi
 
 # Define pins
 forward=24
@@ -16,24 +17,26 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 # Set pins 23, 24, and 26 as inputs with initial value to be pulled high (ON)
 GPIO.setup(backward, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(foreward, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(forward, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(pause, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # Pin 25 is ground
 
 # Callback functions for buttons
 def backwardCallback(channel):
   print("Backward button was pushed!")
+  sapi.prev()
 
 def forwardCallback(channel):
   print("Forward button was pushed!")
+  sapi.next()
 
 def playPauseCallback(channel):
   print("Play/Pause button was pushed!")
 
 # Register callback functions
 GPIO.add_event_detect(backward, GPIO.FALLING,callback=backwardCallback)
-GPIO.add_event_detect(backward, GPIO.FALLING,callback=backwardCallback)
-GPIO.add_event_detect(backward, GPIO.FALLING,callback=backwardCallback)
+GPIO.add_event_detect(forward, GPIO.FALLING,callback=backwardCallback)
+GPIO.add_event_detect(pause, GPIO.FALLING,callback=playPauseCallback)
 
 # pseudo main loop
 message = input("Press enter to quit\n")
---
+++ sapi.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import requests
+import subprocess
+
+# >>> ip = subprocess.check_output(['hostname', '-I'])
+# >>> ip
+# '10.0.0.232 \n'
+# >>> ip.strip()
+# '10.0.0.232'
+# >>> 
+
+payload = {'accept':'application/json'}
+control_url = ""
+next_url = ""
+prev_url = ""
+
+def get_ip():
+    ip = subprocess.check_output(['hostname', '-I'])
+    return ip.strip()
+
+def setup():
+    global control_url
+    global next_url
+    global prev_url
+
+    ip = get_ip()
+
+    control_url = "http://" + ip + "/api/v1/assets/control/"
+    next_url = control_url + "next"
+    prev_url = control_url + "previous"
+
+def action(url):
+    if url != "":
+        r = requests.get(url, payload)
+        print r.json()
+        del r
+    else:
+        print "Call setup() with the ip address of the raspberry pi"
+
+def next():
+    action(next_url)
+
+def prev():
+    action(prev_url)
+
+# Do the setup automatically
+setup()
