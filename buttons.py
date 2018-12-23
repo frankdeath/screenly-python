0dfdf65b43a15b5527091a4c0c2cb5e52cd755ed
Kevin Peterson <frankdeath@gmail.com>
2018-12-22 22:30:52 -0600

    Added a real main loop and the ability to exit gracefully.

--- buttons.py
+++ buttons.py
@@ -5,21 +5,18 @@
 
 import RPi.GPIO as GPIO
 import sapi
+import signal
+import time
 
-# Define pins
-backward=37
-forward=38
-# Pin 39 is ground
-pause=40
+# Allow the script to exit gracefully to insure cleanup is done
+class GracefulExit:
+  def __init__(self):
+    self.timeToExit = False
+    signal.signal(signal.SIGINT, self.exitNow)
+    signal.signal(signal.SIGTERM, self.exitNow)
 
-# Ignore warnings
-GPIO.setwarnings(False)
-# Use the board numbering scheme for I/O pins
-GPIO.setmode(GPIO.BOARD)
-# Set pins as inputs with initial value to be pulled high (ON)
-GPIO.setup(backward, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(forward, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(pause, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  def exitNow(self, signum, frame):
+    self.timeToExit = True
 
 # Callback functions for buttons
 def backwardCallback(channel):
@@ -34,13 +31,38 @@ def playPauseCallback(channel):
   print("Play/Pause button was pushed!")
   sapi.pause()
 
-# Register callback functions
-GPIO.add_event_detect(backward, GPIO.FALLING,callback=backwardCallback)
-GPIO.add_event_detect(forward, GPIO.FALLING,callback=forwardCallback)
-GPIO.add_event_detect(pause, GPIO.FALLING,callback=playPauseCallback)
+def setupGPIO():
+  # Define pins
+  backward=37
+  forward=38
+  # Pin 39 is ground
+  pause=40
 
-# pseudo main loop
-message = input("Press enter to quit\n")
+  # Ignore warnings
+  GPIO.setwarnings(False)
 
-# Clean up
-GPIO.cleanup()
+  # Use the board numbering scheme for I/O pins
+  GPIO.setmode(GPIO.BOARD)
+
+  # Set pins as inputs with initial value to be pulled high (ON)
+  GPIO.setup(backward, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(forward, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(pause, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+  # Register callback functions
+  GPIO.add_event_detect(backward, GPIO.FALLING,callback=backwardCallback)
+  GPIO.add_event_detect(forward, GPIO.FALLING,callback=forwardCallback)
+  GPIO.add_event_detect(pause, GPIO.FALLING,callback=playPauseCallback)
+
+if __name__ == '__main__':
+  ge = GracefulExit()
+
+  setupGPIO()
+
+  while not ge.timeToExit:
+    # Sleep for a relatively long time to minimize cpu usage, but still be responsive
+    sleep(1e-2)
+
+  # Clean up
+  print("Cleaning up!")
+  GPIO.cleanup()
